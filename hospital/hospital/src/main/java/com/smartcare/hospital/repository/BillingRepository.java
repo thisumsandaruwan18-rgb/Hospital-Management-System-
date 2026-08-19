fd9b511bc73fa2ab0141ccad049eff3a43cab230
Thisum Sandaruwan <thisumsandaruwan18@gmail.com>
2026-08-19 11:00:33 +0530

    Delete hospital/hospital directory

--- hospital/hospital/src/main/java/com/smartcare/hospital/repository/BillingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcare.hospital.repository;
-
-import com.smartcare.hospital.entity.Billing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billing, Integer> {
-}
--- hospital/hospital/src/main/java/com/smartcare/hospital/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcare.hospital.repository;
-
-import com.smartcare.hospital.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department,Integer> {
-}
